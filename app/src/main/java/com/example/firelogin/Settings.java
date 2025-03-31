620676c6af7aebd7cd302ef1f13f844f0aab904d
Carlos Arias <carlosariassandoval34@gmail.com>
2025-03-31 10:00:46 -0600

    redirecciones

--- app/src/main/java/com/example/firelogin/Settings.java
+++ app/src/main/java/com/example/firelogin/Settings.java
@@ -2,13 +2,30 @@ package com.example.firelogin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+
+import java.util.HashMap;
 
 public class Settings extends AppCompatActivity {
 
+    LinearLayout btnNotification;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
+
+        btnNotification = (LinearLayout) findViewById(R.id.notification);
+
+        btnNotification.setOnClickListener(view ->  {
+                Intent intent = new Intent(Settings.this, Settings_Notifications.class);
+                startActivity(intent);
+
+        });
     }
 }
---
+++ app/src/main/java/com/example/firelogin/Settings_Notifications.java
@@ -0,0 +1,14 @@
+package com.example.firelogin;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Settings_Notifications extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_notifications);
+    }
+
+}
