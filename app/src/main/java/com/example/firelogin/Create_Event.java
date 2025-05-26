d3974ce71ca6f62dc71b4e9c71b51f9e3951ffe9
Carlos Arias <carlosariassnadoval34@gmail.com>
2025-05-25 19:24:31 -0600

    muestra marcadores en mapa y crea el evento con ubicacion marcada en mapa de igual forma

--- app/src/main/java/com/example/firelogin/Create_Event.java
+++ app/src/main/java/com/example/firelogin/Create_Event.java
@@ -1,18 +1,31 @@
 package com.example.firelogin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.Manifest;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
@@ -20,14 +33,32 @@ import android.widget.Spinner;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
-import com.example.firelogin.ui.events.EventsFragment;
+import com.example.firelogin.ui.groups.GroupsViewModel;
 import com.google.firebase.Timestamp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.GeoPoint;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.osmdroid.api.IMapController;
+import org.osmdroid.events.MapEventsReceiver;
+import org.osmdroid.tileprovider.tilesource.OnlineTileSourceBase;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.util.MapTileIndex;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.CopyrightOverlay;
+import org.osmdroid.views.overlay.MapEventsOverlay;
+import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -39,6 +70,14 @@ import java.util.Locale;
 import java.util.Map;
 
 public class Create_Event extends AppCompatActivity implements View.OnClickListener {
+
+    private MapView map;
+    private IMapController mapController;
+    private static final String TAG = "OsmActivity";
+    private static final int PERMISSION_REQUEST_CODE = 1;
+
+    AutoCompleteTextView searchEditText;
+    Button searchButton, waypointBtn;
 
     Calendar calendar;
     EditText etEventName, etEventDescrip, etEventLat, etEventLong, etEventDate, etEventEndDate, etEventMaterials, etEventMinVolun;
@@ -57,8 +96,6 @@ public class Create_Event extends AppCompatActivity implements View.OnClickListe
         etEventName = findViewById(R.id.etEventName);
         spEventType = findViewById(R.id.spEventType);
         etEventDescrip = findViewById(R.id.etEventDescrip);
-        etEventLat = findViewById(R.id.etEventLat);
-        etEventLong = findViewById(R.id.etEventLong);
         etEventDate = findViewById(R.id.etEventDate);
         etEventEndDate = findViewById(R.id.etEventEndDate);
         etEventMaterials = findViewById(R.id.etEventMaterials);
@@ -146,15 +183,24 @@ public class Create_Event extends AppCompatActivity implements View.OnClickListe
             spEventType.setAdapter(adapter);
         });
 
+        GroupsViewModel groupsViewModel =
+                new ViewModelProvider(this).get(GroupsViewModel.class);
+
+        if (isStoragePermissionGranted()) {
+            setupMap();
+            setupSearch();
+            Log.w(TAG, "aca ando wey");
+        }
 
         btnSaveEvent.setOnClickListener(this);
+
 
     }
 
 
     @Override
     public void onClick(View v) {
-        if (checkFields(etEventName, spEventType, etEventDescrip, etEventLat, etEventLong, etEventDate, etEventEndDate, etEventMaterials,
+        if (checkFields(etEventName, spEventType, etEventDescrip, etEventDate, etEventEndDate, etEventMaterials,
                 etEventMinVolun)) {
 
             EventType selectedType = (EventType) spEventType.getSelectedItem();
@@ -163,7 +209,6 @@ public class Create_Event extends AppCompatActivity implements View.OnClickListe
                 data.put("event_name", etEventName.getText().toString().trim());
                 data.put("type", db.collection("event_type").document(selectedType.getId().toString()));
                 data.put("description", etEventDescrip.getText().toString().trim());
-                data.put("location", new GeoPoint(Double.parseDouble(etEventLat.getText().toString()), Double.parseDouble(etEventLong.getText().toString())));
                 data.put("date", new Timestamp(sdf.parse(etEventDate.getText().toString())));
                 data.put("end_date", new Timestamp(sdf.parse(etEventEndDate.getText().toString())));
                 data.put("materials", etEventMaterials.getText().toString().trim());
@@ -233,7 +278,7 @@ public class Create_Event extends AppCompatActivity implements View.OnClickListe
         etDate.setText(sdf.format(calendar.getTime()));
     }
 
-    public boolean checkFields(EditText name, Spinner type, EditText descrip, EditText latitude, EditText longitude, EditText date,
+    public boolean checkFields(EditText name, Spinner type, EditText descrip, EditText date,
                                EditText endDate, EditText materials, EditText volun) {
 
         if (name.getText().toString().trim().isEmpty()) return false;
@@ -241,12 +286,6 @@ public class Create_Event extends AppCompatActivity implements View.OnClickListe
         if (type.getSelectedItem().toString().isEmpty()) return false;
 
         if (descrip.getText().toString().trim().isEmpty()) return false;
-
-        if (latitude.getText().toString().trim().isEmpty() || Double.parseDouble(latitude.getText().toString()) > 90
-                || Double.parseDouble(latitude.getText().toString()) < -90) return false;
-
-        if (longitude.getText().toString().trim().isEmpty() || Double.parseDouble(longitude.getText().toString()) > 180
-                || Double.parseDouble(longitude.getText().toString()) < -180) return false;
 
         if (materials.getText().toString().trim().isEmpty()) return false;
 
@@ -278,5 +317,304 @@ public class Create_Event extends AppCompatActivity implements View.OnClickListe
     public boolean onSupportNavigateUp() {
         finish();
         return true;
+    }
+
+    public boolean isStoragePermissionGranted() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{
+                            android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                            android.Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION
+                    },
+                    PERMISSION_REQUEST_CODE);
+            Log.w(TAG, "se aceptaron permisos");
+
+            return true;
+        } else {
+            Log.w(TAG, "valio madre");
+
+        }
+        Log.w(TAG, "Se aceptaron permisos");
+
+        return true;
+    }
+
+    private MapEventsOverlay currentEventOverlay = null;
+    private boolean isWaitingForTap = false;
+
+    private void setupMap() {
+
+        map = findViewById(R.id.mapView);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        map.setBuiltInZoomControls(true);
+        map.setMultiTouchControls(true);
+        map.getOverlayManager().add(new CopyrightOverlay(this));
+
+
+        mapController = map.getController();
+        mapController.setZoom(18);
+
+        map.setTileSource(new OnlineTileSourceBase(
+                "Carto Light",
+                1, 20, 256, "",
+                new String[] { "a", "b", "c" }) {
+
+            @Override
+            public String getTileURLString(final long pMapTileIndex) {
+                int zoom = MapTileIndex.getZoom(pMapTileIndex);
+                int x = MapTileIndex.getX(pMapTileIndex);
+                int y = MapTileIndex.getY(pMapTileIndex);
+
+
+                return "https://" + getBaseUrl() + ".basemaps.cartocdn.com/light_all/"
+                        + zoom + "/" + x + "/" + y + ".png";
+            }
+        });
+        map.setMultiTouchControls(true);
+
+        Log.w(TAG, "ando aca");
+
+
+        if (isStoragePermissionGranted() == true) {
+
+            MyLocationNewOverlay mLocationOverlay = new MyLocationNewOverlay(
+                    new GpsMyLocationProvider(this), map);
+            mLocationOverlay.enableMyLocation();
+            mLocationOverlay.enableFollowLocation();
+            map.getOverlays().add(mLocationOverlay);
+
+            mLocationOverlay.runOnFirstFix(() -> {
+                org.osmdroid.util.GeoPoint myLocation = mLocationOverlay.getMyLocation();
+                Log.d(TAG, "MyLocation: " + myLocation);
+                if (myLocation != null) {
+                    this.runOnUiThread(() -> {
+                        mapController.setCenter(myLocation);
+
+                    });
+                } else {
+                    Log.w(TAG, "No se puede optener la ubicación");
+                }
+
+            });
+        }
+
+        Button waypoint = findViewById(R.id.waypointBtn);
+
+        waypoint.setOnClickListener(v-> {
+            Log.w(TAG,"si detecto el clic en el boton");
+
+            if (isWaitingForTap) return;
+
+            if (currentEventOverlay != null) {
+                map.getOverlays().remove(currentEventOverlay);
+                map.invalidate();
+            }
+
+            isWaitingForTap = true;
+
+            MapEventsReceiver mReceive = new MapEventsReceiver() {
+
+                @Override
+                public boolean singleTapConfirmedHelper(org.osmdroid.util.GeoPoint p) {
+                    org.osmdroid.util.GeoPoint startPoint = new org.osmdroid.util.GeoPoint(p.getLatitude(), p.getLongitude());
+
+                    Marker waypoint = new Marker(map);
+                    waypoint.setPosition(startPoint);
+                    waypoint.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+                    map.getOverlays().add(waypoint);
+                    map.invalidate();
+
+                    Log.w(TAG, "Clic detectado en: " + p.getLatitude() + ", " + p.getLongitude());
+
+                    map.getOverlays().remove(currentEventOverlay);
+                    currentEventOverlay = null;
+                    isWaitingForTap = false;
+
+                    return true;
+                }
+
+                @Override
+                public boolean longPressHelper(org.osmdroid.util.GeoPoint p) {
+                    return true;
+                }
+            };
+
+            currentEventOverlay = new MapEventsOverlay(mReceive);
+            map.getOverlays().add(currentEventOverlay);
+            map.invalidate();
+        });
+
+    }
+
+    private void setupSearch() {
+
+        AutoCompleteTextView searchEditText = findViewById(R.id.searchEditText);
+        Button searchButton = findViewById(R.id.searchButton);
+
+        searchEditText.addTextChangedListener(new TextWatcher() {
+            @Override public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.length() > 2) {
+                    fetchSuggestions(s.toString());
+                }
+            }
+            @Override public void afterTextChanged(Editable s) {}
+        });
+
+        searchEditText.setOnItemClickListener((parent, view, position, id) -> {
+            String selectedText = (String) parent.getItemAtPosition(position);
+            searchLocation(selectedText);
+        });
+
+        searchButton.setOnClickListener(v -> {
+            String locationName = searchEditText.getText().toString().trim();
+            if (!locationName.isEmpty()) {
+                searchLocation(locationName);
+            }
+        });
+    }
+
+    private void fetchSuggestions(String locationName ) {
+        new Thread(() -> {
+            try {
+                String urlStr = "https://photon.komoot.io/api/?q=" +
+                        URLEncoder.encode(locationName, "UTF-8") + "&limit=5";
+
+
+                URL url = new URL(urlStr);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.connect();
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                StringBuilder result = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) result.append(line);
+
+                JSONObject jsonObject = new JSONObject(result.toString());
+                JSONArray features = jsonObject.getJSONArray("features");
+
+                List<String> suggestions = new ArrayList<>();
+                for (int i = 0; i < features.length(); i++) {
+                    JSONObject feature = features.getJSONObject(i);
+                    JSONObject properties = feature.getJSONObject("properties");
+                    String label = properties.getString("name");
+                    String city = properties.optString("city", "");
+                    String fullLabel = city.isEmpty() ? label : label + ", " + city;
+                    suggestions.add(fullLabel);
+                }
+
+                this.runOnUiThread(() -> {
+                    ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, suggestions);
+                    AutoCompleteTextView searchEditText = findViewById(R.id.searchEditText);
+                    searchEditText.setAdapter(adapter);
+                    adapter.notifyDataSetChanged();
+                });
+
+
+                if (features.length() > 0) {
+                    JSONObject firstFeature = features.getJSONObject(0);
+                    JSONObject geometry = firstFeature.getJSONObject("geometry");
+                    JSONArray coordinates = geometry.getJSONArray("coordinates");
+                    Log.d(TAG, "ni entra aca1");
+                    double lon = coordinates.getDouble(0);
+                    double lat = coordinates.getDouble(1);
+
+                    org.osmdroid.util.GeoPoint point = new org.osmdroid.util.GeoPoint(lat, lon);
+                    this.runOnUiThread(() -> {
+                        mapController.setCenter(point);
+                        mapController.setZoom(18);
+                        Log.d(TAG, "ni entra aca2");
+                    });
+
+                } else {
+                    Log.d(TAG, "Ubicación no encontrada");
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                Log.d(TAG, "Valio madre");
+            }
+            Log.d(TAG, "ni entra aca3");
+        }).start();
+    }
+
+    private void searchLocation(String locationName) {
+        new Thread(() -> {
+            try {
+                String email = (user != null) ? user.getEmail() : "unknown_user@example.com";
+
+                String urlStr = "https://photon.komoot.io/api/?q=" +
+                        URLEncoder.encode(locationName, "UTF-8") + "&limit=1";
+
+                URL url = new URL(urlStr);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestProperty("User-Agent", "FireLoginApp/1.0 (" + email + ")");
+                conn.connect();
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                StringBuilder result = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) result.append(line);
+
+                JSONObject jsonObject = new JSONObject(result.toString());
+                JSONArray features = jsonObject.getJSONArray("features");
+
+                if (features.length() > 0) {
+                    JSONObject geometry = features.getJSONObject(0).getJSONObject("geometry");
+                    JSONArray coordinates = geometry.getJSONArray("coordinates");
+                    double lon = coordinates.getDouble(0);
+                    double lat = coordinates.getDouble(1);
+                    org.osmdroid.util.GeoPoint point = new GeoPoint(lat, lon);
+
+                    this.runOnUiThread(() -> {
+                        mapController.setCenter(point);
+                        mapController.setZoom(18);
+                    });
+                } else {
+                    Log.d("Location", "Ubicación no encontrada");
+                }
+
+            } catch (Exception e) {
+                Log.e("Location", "Error buscando ubicación", e);
+            }
+        }).start();
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (map != null)
+            map.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (map != null)
+            map.onPause();
+    };
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            boolean allGranted = true;
+            for (int i = 0; i < permissions.length; i++) {
+                Log.d(TAG, "Permiso " + permissions[i] + ": " + grantResults[i]);
+                if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                    allGranted = false;
+                }
+            }
+
+            if (allGranted) {
+                Log.v(TAG, "Permisos otorgados correctamente");
+            }
+        }
     }
 }
