f16717fe6640300a9b96eb8596f5208c0a318bc1
martin <martnalej@gmail.com>
2025-05-27 05:59:11 -0600

    avance en el calendario(no sirve)

--- app/src/main/java/com/example/firelogin/ui/calendar/CalendarFragment.java
+++ app/src/main/java/com/example/firelogin/ui/calendar/CalendarFragment.java
@@ -1,5 +1,8 @@
 package com.example.firelogin.ui.calendar;
 
+import static com.example.firelogin.StaticFunctions.showToastAlert;
+
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,11 +13,28 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.firelogin.FirebaseHandler;
+import com.example.firelogin.R;
 import com.example.firelogin.databinding.FragmentCalendarBinding;
 import com.example.firelogin.ui.calendar.CalendarViewModel;
+import com.google.firebase.firestore.Query;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 public class CalendarFragment extends Fragment {
     private FragmentCalendarBinding binding;
+    MaterialCalendarView calendarView;
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm", Locale.getDefault());
+    List<EventDecorator> dates;
+    private int lastDateIndex=0;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -26,9 +46,50 @@ public class CalendarFragment extends Fragment {
 
         final TextView textView = binding.textCalendar;
         calendarViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        calendarView = root.findViewById(R.id.eventsCalendar);
+        dates=new ArrayList<>();
+        FirebaseHandler fb= new FirebaseHandler(2);
+        fb.db.collection("eventos").orderBy("date", Query.Direction.DESCENDING).get()
+                .addOnSuccessListener(task->{
+                    task.getDocuments().forEach(item->{
+                        String date = sdf.format(item.getDate("date"));
+                        setEventDate(date,item.getId());
+                    });
+                });
+        calendarView.setOnDateChangedListener((widget,selectedDate,selected)->{
+            showToastAlert(requireContext(),"DATE SELECTED:"+selectedDate.toString());
+        });
         return root;
     }
+    private void setEventDate(String formatedDate,String eventId){
+        if(!dates.isEmpty()) {
+            for (int index = lastDateIndex; index < dates.size(); index++) {
+                EventDecorator dateDecorator = dates.get(index);
+                int dateReference = dateDecorator.isFromDate(formatedDate);
+                if (dateReference == 1) {
+                    dateDecorator.addEvent(eventId);
+                    lastDateIndex = index;
+                    return;
+                }
+            }
+        }
+        //no existe la fecha, es decir, es nueva para el calendario
 
+        String[] splitedDate=formatedDate.split("/");// DD-MM-YYYY
+        Calendar calendar=Calendar.getInstance();
+        calendar.set(
+                Integer.parseInt(splitedDate[2]),
+                Integer.parseInt(splitedDate[1]),
+                Integer.parseInt(splitedDate[0]));
+        CalendarDay date = CalendarDay.from(calendar);
+        EventDecorator decorator = new EventDecorator(Color.GREEN,
+                Collections.singleton(date),
+                formatedDate,eventId);
+        dates.add(decorator);
+        calendarView.addDecorator(decorator);
+        lastDateIndex=dates.size()-1;
+    }
     @Override
     public void onDestroyView() {
         super.onDestroyView();
---
+++ app/src/main/java/com/example/firelogin/ui/calendar/EventDecorator.java
@@ -0,0 +1,83 @@
+package com.example.firelogin.ui.calendar;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.DayViewDecorator;
+import com.prolificinteractive.materialcalendarview.DayViewFacade;
+import com.prolificinteractive.materialcalendarview.spans.DotSpan;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+
+public class EventDecorator implements DayViewDecorator {
+    private final int color;
+    private final HashSet<CalendarDay> dates;
+    List<String> eventsId;
+    String dateString;
+    public EventDecorator(int color, Collection<CalendarDay> dates,String dateString,String eventId){
+        this.color=color;
+        this.dates=new HashSet<>(dates);
+        eventsId=new ArrayList<>();
+        addEvent(eventId);
+        this.dateString=dateString;
+    }
+    @Override
+    public boolean shouldDecorate(CalendarDay day) {
+        return dates.contains(day);
+    }
+
+    @Override
+    public void decorate(DayViewFacade view) {
+        view.addSpan(new DotSpan(10,color));
+    }
+    public void addEvent(String eventId){
+        eventsId.add(eventId);
+    }
+    public Boolean containsEvent(String eventId){
+        return eventsId.contains(eventId);
+    }
+    public int isFromDate(String date){
+        if(dateString.equals(date)){
+            return 1;
+        }
+        return (isBeforeMyDate(date))?-1:-2;
+    }
+    public Boolean isBeforeMyDate(String date){
+        int[] subDate = splitDate(date);
+        int[] subMyDate = splitDate(dateString);
+        if(subDate[2] < subMyDate[2]){
+            // pregunton es de antes de mi año
+            return true;
+        }
+        if(subDate[2]>subMyDate[2]){
+            // pregunton despues de mi año
+            return false;
+        }
+        //mismo año
+        if(subDate[1]<subMyDate[1]){
+            // pregunton es de antes de mi mes
+            return true;
+        }if(subDate[1]>subMyDate[1]){
+            // pregunton es despues de mi mes
+            return false;
+        }
+        //mismo mes
+        if(subDate[0]<=subMyDate[0]){
+            // pregunton es de antes de mi mes
+            return true;
+        }
+        return false;
+    }
+    private int[] splitDate(String date){
+        String[] subDate = date.split("/");
+        return new int[]{
+                parseInt(subDate[0]),
+                parseInt(subDate[1]),
+                parseInt(subDate[2])
+        };
+    }
+    private int parseInt(String value){
+        return Integer.parseInt(value);
+    }
+}
